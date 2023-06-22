6600102a6bce174f06c62cfdf81b028a522cb4fd
yurilabatsevich <yurilabatsevich@gmail.com>
2023-06-22 23:38:19 +0300

    refactor: change module name from  OSInfo to info

---
+++ src/info.js
@@ -0,0 +1,35 @@
+import { fontColor as fc, resetColor as rc } from './settings.js';
+import { stdout as output } from 'node:process';
+import os from 'node:os';
+
+class OSInfo {
+
+  eol() {
+    output.write(JSON.stringify(os.EOL));
+  }
+
+   cpus() {
+    const cpus = os.cpus();
+    output.write(`Your host machine has ${this.__cpus.length} CPUS`);
+    os.cpus().forEach((info) => {
+      const { model, speed } = info;
+      output.write(`${fc} \nModel: ${model}, Clock rate: ${speed / 1000}GHz ${rc}`);
+    });
+  }
+
+   architecture() {
+    output.write(`${fc} Operating system CPU architecture: ${os.arch()} ${rc} \n`);
+  }
+
+   homedir() {
+    output.write(`${fc} Home directory: ${os.homedir()} ${rc} \n`)
+  }
+
+   username() {
+    const { username } = os.userInfo();
+    output.write(`${fc} Current system user name : ${username} ${rc} \n`)
+  }
+
+}
+
+export default OSInfo;
