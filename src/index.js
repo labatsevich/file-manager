c38897c730f6dcbb4d8321381480cbee75de53a8
yurilabatsevich <yurilabatsevich@gmail.com>
2023-06-22 23:44:01 +0300

    refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -2,56 +2,77 @@ import { stdin as input, stdout as output, stdout } from 'node:process';
 import path from 'node:path';
 import os from 'node:os';
 import readline from 'node:readline/promises';
-import OSInfo from './OSInfo.js';
-import Navigation from './Navigation.js';
+import OSInfo from './info.js';
+import Navigation from './navigation.js';
 
 let username;
-const __currentDir = os.homedir();
-const __cliArguments = process.argv.slice(2).join('');
+let currentDir = os.homedir();
+const cliArgs = process.argv.slice(2).join('');
 
-const __os = new OSInfo();
-const __navigator = new Navigation(__currentDir);
+const sysInfo = new OSInfo();
+const navigator = new Navigation(currentDir);
 
 
-if(__cliArguments.indexOf('--username')!==-1){
-    username = __cliArguments.split('=').pop();
-    stdout.write(`\nWelcome to the File Manager, ${username}!\nYou are currently in ${__currentDir}\n`);
+if (cliArgs.indexOf('--username') !== -1) {
+  username = cliArgs.split('=').pop();
+  stdout.write(`\nWelcome to the File Manager, ${username}!\nYou are currently in ${currentDir}\n`);
 }
-else{
+else {
   stdout.write('Incorrect command! \nType \'npm run start -- --username=your_username\'\n and try again!')
   process.exit();
 }
 
 
-const rl = readline.createInterface({input,output});
+const rl = readline.createInterface({ input, output });
 
-rl.on('line',(line) => {
-  
-  if(line === '.exit') { 
+rl.on('line', async (line) => {
+
+  if (line === '.exit') {
     stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`)
     process.exit();
   }
-  
-  if(line.startsWith('os')){
+
+  if (line.startsWith('os')) {
 
     const method = line.slice(5).toLowerCase();
-    try{
-     __os[method]();
-    }catch{
+    try {
+      sysInfo[method]();
+      stdout.write(`\nYou are currently in ${currentDir}\n`);
+    } catch {
       stdout.write('Invalid command\n');
     }
   }
 
 
-  if(line.startsWith('cd')){
-    const method    = line.slice(0,2).toLowerCase();
-    const dir_path  = line.slice(3).replace(/--/,''); 
-    __navigator[method](dir_path);
+  if (line.startsWith('cd')) {
+
+    const method = line.slice(0, 2).toLowerCase();
+    const params = line.split(' ');
+    let newDir = params[params.length - 1];
+
+    try {
+      currentDir = await navigator.cd(currentDir, newDir);
+    } catch (err) {
+      output.write(err)
+    }
+
+    stdout.write(`\nYou are currently in ${currentDir}\n`);
+
+  }
+
+  if (line.startsWith('up')) {
+    currentDir = navigator.up(currentDir);
+    stdout.write(`\nYou are currently in ${currentDir}\n`);
+  }
+
+  if (line.startsWith('ls')) {
+    await navigator.ls(currentDir);
+    stdout.write(`\nYou are currently in ${currentDir}\n`);
   }
 
 });
 
-rl.on('SIGINT', () => { 
+rl.on('SIGINT', () => {
   stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`);
   rl.close();
 });
