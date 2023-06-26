506d1652f1e2ae30cdb6005a4f0b9b0a1db18b4a
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-26 14:40:23 +0300

    feat: add function to compress and decompress

--- src/brotli.js
+++ src/brotli.js
@@ -1,3 +1,47 @@
-export const calculate = async(pathToFile) =>{
+import {createReadStream, createWriteStream} from 'node:fs'
+import { isExists, isFile } from './helpers.js';
+import { pipeline } from 'node:stream';
+import {stdout as output} from 'node:process';
+import { MESSAGE } from './settings.js';
+import { createBrotliCompress, createBrotliDecompress } from 'node:zlib';
 
+export const compress = async(pathToFile, pathToDest) => {
+
+    const existsSrc = await isExists(pathToFile);
+    const existsDest = await isExists(pathToDest);
+
+    if(existsSrc && isFile(pathToFile) && !existsDest){
+        const readable = createReadStream(pathToFile);
+        const writeble = createWriteStream(pathToDest);
+        const brotli = createBrotliCompress();
+        await pipeline(readable,brotli,writeble,(err)=> { 
+            if(err)output.write(`${MESSAGE.FAILED}\n`)
+        });
+        output.write(`${MESSAGE.SUCCESS}`)
+    }
+    else{
+        output.write(`${MESSAGE.FAILED}\n`)
+    }
+
+};
+
+export const decompress = async(pathToFile, pathToDest) => {
+
+    const existsSrc = await isExists(pathToFile);
+    const existsDest = await isExists(pathToDest);
+
+        if(existsSrc && isFile(pathToFile) && !existsDest){
+        const readable = createReadStream(pathToFile);
+        const writeble = createWriteStream(pathToDest);
+        const brotli = createBrotliDecompress();
+        await pipeline(readable,brotli,writeble,(err)=> { 
+            if(err)output.write(`${MESSAGE.FAILED}\n`)
+        });
+        output.write(`${MESSAGE.SUCCESS}`)
+    }
+    else{
+        output.write(`${MESSAGE.FAILED}\n`)
+    }
+
+    
 };
